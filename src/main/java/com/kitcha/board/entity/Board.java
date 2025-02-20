ad82e8a6b988423c38fcb5507495f6315f071eb0
DESKTOP-LD83KVF\user <kmj13675@gmail.com>
2025-02-20 22:32:03 +0900

    Add board Entity

--- src/main/java/com/kitcha/board/entity/Board.java
+++ src/main/java/com/kitcha/board/entity/Board.java
@@ -1,4 +1,47 @@
 package com.kitcha.board.entity;
 
-public class board {
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "board")
+@Data
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long boardId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private int hitCnt = 0;
+
+    @Column(nullable = false)
+    private String summary;
+
+    @Column(nullable = false)
+    private String url;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(nullable = false)
+    private boolean deletedYn = false;
+
+    @Column
+    private Long userId;
+
 }
